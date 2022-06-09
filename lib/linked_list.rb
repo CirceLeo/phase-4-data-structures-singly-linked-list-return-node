5cdc66be4603c44e0118ecf143a7264e2f7d8d2d
Calypso Leonard <calypsorleonard@gmail.com>
2022-06-09 18:53:57 -0400

    commit tree f5981ac11a84af357763dd135472a766908175e1

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -7,8 +7,46 @@ class LinkedList
     @head = nil
   end
 
+  # def nth_from_end(n)
+  #   counter = 0 
+  #   current = @head
+  #   until current.nil?
+  #     current = current.next_node
+  #     counter = counter + 1
+  #   end
+
+  #   return nil if n < 1 || n > counter
+
+  #   node = head
+  #   i = 1 
+  #   while i < counter - n + 1
+  #     node = node.next_node
+  #     i +=  1
+  #   end 
+  #   node.value
+  # end
+
   def nth_from_end(n)
-    # your code here
+
+    return nil if head.nil? || n < 1
+
+    n_from_back = head
+    leader = head
+
+    counter = 0
+    while counter < n
+      return nil if leader.nil?
+
+      leader = leader.next_node
+      counter += 1
+    end
+
+    until leader.nil?
+      leader = leader.next_node
+      n_from_back = n_from_back.next_node
+    end
+  
+    n_from_back.value
   end
 
 end
